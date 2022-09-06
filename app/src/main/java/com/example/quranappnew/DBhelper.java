5ba30730ec9b56975a8e57f98b646baab7190b26
hurmainn <hurmainjavaid18@gmail.com>
2022-09-06 23:12:41 +0500

    list view done

---
+++ app/src/main/java/com/example/quranappnew/DBhelper.java
@@ -0,0 +1,2 @@
+package com.example.quranappnew;public class DBHelper {
+}
---
+++ app/src/main/java/com/example/quranappnew/SurahList.java
@@ -0,0 +1,14 @@
+package com.example.quranappnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SurahList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_surah_list);
+    }
+}
